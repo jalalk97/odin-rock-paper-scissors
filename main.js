a065a101a5a28308e301ca119c020052738d0dd7
Jalal <jalal.kalyati@gmail.com>
2023-07-03 23:16:57 -0400

    Add gui and modify game logic functions to modify the DOM

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ const LOSS = -1;
 const DRAW = 0;
 const NUMBER_OF_GAMES = 5;
 
+const buttons = [
+  document.querySelector("#rock-button"),
+  document.querySelector("#paper-button"),
+  document.querySelector("#scissors-button"),
+];
+const scoreDiv = document.querySelector("#score");
+const announcementDiv = document.querySelector("#announcement");
+const restartButton = document.querySelector("#restart-button");
+
+let wins;
+let losses;
+
+resetGame();
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -17,93 +31,125 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function winMessage(playerSelection, computerSelection) {
+  return `You Win! ${playerSelection} beats ${computerSelection}`;
+}
+
+function lossMessage(playerSelection, computerSelection) {
+  return `You Lose! ${playerSelection} loses to ${computerSelection}`;
+}
+
+function drawMessage() {
+  return "It's a draw!";
+}
+
+function errorMessage(selection) {
+  return `Invalid option: ${selection}`;
+}
+
 function playRound(playerSelection, computerSelection) {
   playerSelection = capitalize(playerSelection);
+  computerSelection = capitalize(computerSelection);
   switch (computerSelection) {
     case "Rock":
       switch (playerSelection) {
         case "Rock":
-          console.log("It's a draw!");
           return DRAW;
         case "Paper":
-          console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
           return WIN;
         case "Scissors":
-          console.log(
-            `You Lose! ${computerSelection} beats ${playerSelection}`
-          );
           return LOSS;
         default:
-          throw new Error(`Invalid option: ${playerSelection}`);
+          throw new Error(errorMessage(playerSelection));
       }
 
     case "Paper":
       switch (playerSelection) {
         case "Rock":
-          console.log(
-            `You Lose! ${computerSelection} beats ${playerSelection}`
-          );
           return LOSS;
         case "Paper":
-          console.log("It's a draw!");
           return DRAW;
         case "Scissors":
-          console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
           return WIN;
         default:
-          throw new Error(`Invalid option: ${playerSelection}`);
+          throw new Error(errorMessage(playerSelection));
       }
 
     case "Scissors":
       switch (playerSelection) {
         case "Rock":
-          console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
           return WIN;
         case "Paper":
-          console.log(
-            `You Lose! ${computerSelection} beats ${playerSelection}`
-          );
           return LOSS;
         case "Scissors":
-          console.log("It's a draw!");
           return DRAW;
         default:
-          throw new Error(`Error: Invalid option: ${playerSelection}`);
+          throw new Error(errorMessage(playerSelection));
       }
 
     default:
-      throw new Error(`Error: Invalid option: ${computerSelection}`);
+      throw new Error(errorMessage(computerSelection));
   }
 }
 
-function game() {
-  let wins = 0;
-  let losses = 0;
-  while (true) {
-    const playerSelection = capitalize(prompt("Your move: "));
-    const computerSelection = getComputerChoice();
-    console.log(`Your move: ${playerSelection}`);
-    console.log(`The computer's move: ${computerSelection}`);
-    try {
-      const result = playRound(playerSelection, computerSelection);
-      if (result == LOSS) {
-        losses++;
-      } else if (result == WIN) {
-        wins++;
-      }
-      console.log(`Score: Player ${wins} - ${losses} Computer\n`);
-      if (wins == NUMBER_OF_GAMES) {
-        console.log("Game Over. You Won!");
-        break;
-      }
-      if (losses == NUMBER_OF_GAMES) {
-        console.log("Game Over. You Lost.");
-        break;
-      }
-    } catch (error) {
-      console.log(error.message);
+function isGameOver() {
+  return wins == NUMBER_OF_GAMES || losses == NUMBER_OF_GAMES;
+}
+
+function displayScore() {
+  scoreDiv.textContent = `${wins} - ${losses}`;
+}
+
+function resetGame() {
+  wins = 0;
+  losses = 0;
+  displayScore();
+  POSSIBLE_MOVES.forEach((_, i) => {
+    buttons[i].disabled = false;
+  });
+  restartButton.classList.add("hidden");
+  announcementDiv.textContent = "";
+}
+
+const handleMove = (playerSelection) => () => {
+  if (isGameOver()) {
+    return;
+  }
+
+  const computerSelection = getComputerChoice();
+  const result = playRound(playerSelection, computerSelection);
+  if (result == WIN) {
+    wins++;
+    announcementDiv.textContent = winMessage(
+      playerSelection,
+      computerSelection
+    );
+  } else if (result == LOSS) {
+    losses++;
+    announcementDiv.textContent = lossMessage(
+      playerSelection,
+      computerSelection
+    );
+  } else if (result == DRAW) {
+    announcementDiv.textContent = drawMessage();
+  }
+  displayScore();
+
+  if (isGameOver()) {
+    if (wins == NUMBER_OF_GAMES) {
+      announcementDiv.textContent = "Game Over, You Won!";
+    } else if (losses == NUMBER_OF_GAMES) {
+      announcementDiv.textContent = "Game Over, You Lost.";
     }
+    POSSIBLE_MOVES.forEach((_, i) => {
+      buttons[i].disabled = true;
+    });
+    restartButton.classList.remove("hidden");
   }
-}
+};
 
-game();
+POSSIBLE_MOVES.forEach((move, i) => {
+  buttons[i].addEventListener("click", handleMove(move));
+});
+
+restartButton.addEventListener("click", resetGame);
